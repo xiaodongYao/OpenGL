97c474287a1269974d9bbc5947d69059c4087b3c
yxd <78114@DESKTOP-8NCR33H>
2023-06-20 17:12:01 +0800

    Setting up OpenGL and Creating a Window in C++

--- ChernoOpenGL/src/Application.cpp
+++ ChernoOpenGL/src/Application.cpp
@@ -9,7 +9,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -24,6 +24,13 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
